637340776670b070c453beaf00665d6d6d715ab6
Your Correct GitHub Username <alieelpozo3@gmail.com>
2025-08-24 14:07:25 -0400

    chore: mcp server

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+{
+  "mcpServers": {
+    "Framelink Figma MCP": {
+      "command": "npx",
+      "args": ["-y", "figma-developer-mcp", "--figma-api-key=figd_-hwfGaizts86wYE9Qpxjp2C0U0zf7DqPYgvb1hzn", "--stdio"]
+    }
+  }
+}
